e986272820966ecce5252d462635db51d43cdf62
Amir <azymy3354@gmail.com>
2025-09-21 15:37:38 +0330

    add feature: send-token as header

--- mobile/RTK/graphql/baseApi.ts
+++ mobile/RTK/graphql/baseApi.ts
@@ -2,11 +2,20 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 import { GraphQLClient } from "graphql-request";
 import { Platform } from "react-native";
+import { RootState } from "../state/store";
 
 const hostname = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
 export const client = new GraphQLClient(`http://${hostname}:4000/graphql`);
 
 export const api = createApi({
-    baseQuery: graphqlRequestBaseQuery({ client }),
+    baseQuery: graphqlRequestBaseQuery({
+        client,
+        prepareHeaders: (headers, { getState }) => {
+            const token = (getState() as RootState).auth.token;
+            token && headers.set("Authorization", token);
+
+            return headers;
+        }
+    }),
     endpoints: build => ({})
 });
